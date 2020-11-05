185a93e43c88294e3bebba62b31477754c2ddd1b
xiejingwei <duoyixjw2020>
2020-11-05 13:38:41 +0800

    longestCommonPrefix_m2

--- src/leetcode/easy/LongestCommonPrefix_14.java
+++ src/leetcode/easy/LongestCommonPrefix_14.java
@@ -6,15 +6,27 @@ package leetcode.easy;
 public class LongestCommonPrefix_14 {
 
     public static void main(String[] args) {
-//        String[] ss = {"flower", "flow", "flight"};
 //        String[] ss = {"dog", "racecar", "car"};
-        String[] ss = {"car"};
+//        String[] ss = {"car"};
 //        String[] ss = {};
-        System.out.println(m1(ss));
+        String[] ss = {"flower", "flow", "flight"};
+        Long start1 = System.currentTimeMillis();
+        for (int i = 1; i < 100 * 10000; i++) {
+            m1(ss);
+        }
+        System.out.println(System.currentTimeMillis() - start1); //400多毫秒
+
+        Long start2 = System.currentTimeMillis();
+        for (int i = 1; i < 100 * 10000; i++) {
+            m2(ss);
+        }
+        System.out.println(System.currentTimeMillis() - start2); //40多毫秒
+
     }
 
     /**
      * 暴力解法
+     * 时间复杂度很高
      *
      * @param ss
      * @return
@@ -41,6 +53,33 @@ public class LongestCommonPrefix_14 {
             ss[i] = common;
         }
         return ss[ss.length - 1];
+    }
+
+    /**
+     * 漂亮解法   和暴力解法的比较次数差不多，但是快了很多。不太理解 todo
+     *
+     * @param ss
+     * @return
+     */
+    public static String m2(String[] ss) {
+
+        if (ss.length == 0) {
+            return "";
+        }
+        String common = ss[0];
+        for (int i = 1; i < ss.length; i++) {
+            int j = 0;
+            for (; j < common.length() && j < ss[i].length(); j++) {
+                if (common.charAt(j) != ss[i].charAt(j)) {
+                    break;
+                }
+            }
+            common = common.substring(0, j);
+            if (common.equals("")) {
+                return common;
+            }
+        }
+        return common;
     }
 
 }
